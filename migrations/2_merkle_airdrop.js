f7c56678a9b462ad9ce2921208097fa73731c861
Apron2050 <xr.frozen@qq.com>
2021-04-19 00:21:55 +0800

    dump abis

--- migrations/2_merkle_airdrop.js
+++ migrations/2_merkle_airdrop.js
@@ -1,6 +1,8 @@
 const MerkleAirdrop = artifacts.require("MerkleAirdrop");
 const Token = artifacts.require("ERC20Token");
 
+const fs = require('fs');
+
 module.exports = async function (deployer, network, accounts) {
 
   deployer.then(async () => {
@@ -8,6 +10,18 @@ module.exports = async function (deployer, network, accounts) {
 
     const token = await Token.deployed();
     const airdrop = await MerkleAirdrop.deployed();
+
+    let contracts = {
+      tokenAddress: token.address,
+      airdropAddress: airdrop.address
+    };
+
+    fs.writeFileSync('./contracts.txt', JSON.stringify(contracts),  {encoding: 'utf-8'});
+
+
+    const airdropBuild = require('../build/contracts/MerkleAirdrop');
+
+    fs.writeFileSync('./MerkleAirdrop.abi.json', JSON.stringify(airdropBuild.abi),  {encoding: 'utf-8'});
 
     await airdrop.setToken(token.address, accounts[0]);
   })
